28a0a2f5dada96736fb18bfc975c1afcfa8bf48c
abdinoor <abdinoor@gmail.com>
2012-11-29 14:50:49 -0500

    Switch to logging statements instead of print. Various pep8 formatting changes.

--- watson/gbot.py
+++ watson/gbot.py
@@ -3,48 +3,49 @@ from wokkel.xmppim import MessageProtocol, AvailablePresence
 from twisted.application import service
 from twisted.words.protocols.jabber import jid
 from wokkel.client import XMPPClient
-
 from watson.chatbot import Chatbot
+import logging
+
 
 class GBotProtocol(MessageProtocol):
     def connectionMade(self):
-        print "Connected!"
+        logging.info("Connected!")
 
         # send initial presence
         self.send(AvailablePresence())
 
     def connectionLost(self, reason):
-        print "Disconnected!"
+        logging.info("Disconnected!")
 
     def onMessage(self, msg):
-        
+
         if msg["type"] == 'chat' and hasattr(msg, "body") and msg.body:
             user = msg["from"]
             message = msg.body
             self.send(AvailablePresence())
             return self.bot.perform_action(user, message)
 
+
 class Gbot(Chatbot):
-        
+
     def __init__(self, name, commands, username, password):
         super(Gbot, self).__init__(name, commands)
         self.username = username
         self.password = password
-        
-        
+
     def speak(self, user, message):
         if not self.protocol:
             print "Must have a protocol before I can speak!"
             return
-        
+
         reply = domish.Element((None, "message"))
         reply["to"] = user
         reply["from"] = self.protocol.parent.jid.full()
         reply["type"] = 'chat'
         reply.addElement("body", content=message)
-        
+
         self.protocol.send(reply)
-    
+
     def connect(self):
         application = service.Application("gbot")
 
@@ -55,11 +56,9 @@ class Gbot(Chatbot):
         self.protocol.setHandlerParent(xmppclient)
         xmppclient.setServiceParent(application)
         return application
-        
-        
 
     def error(self):
         pass
-    
+
     def disconnect(self):
         pass
