1c28678cf055275d3d32e11c496d3e7a70bd7723
abdinoor <abdinoor@gmail.com>
2012-11-29 15:33:28 -0500

    PEP8 formatting. No functional changes.

--- watson/modules/chatmodule.py
+++ watson/modules/chatmodule.py
@@ -14,31 +14,33 @@ def command_function(*syntaxes):
         return f
     return g
 
+
 class ChatModuleMeta(type):
     def __new__(cls, name, bases, dct):
         clss = super(ChatModuleMeta, cls).__new__(cls, name, bases, dct)
-        clss.command_functions = [x for x in dct.values() if hasattr(x,"command_grammars")]
+        clss.command_functions = [x for x in dct.values() if hasattr(x, "command_grammars")]
         return clss
 
+
 class ChatModule(object):
-    __metaclass__ =  ChatModuleMeta
-    
+    __metaclass__ = ChatModuleMeta
+
     __module_name__ = None
     __module_description__ = None
     __module_dependencies__ = []
-    
+
     def __init__(self):
         self.bot = None
-    
+
     def perform_action(self, user, command):
         hit = False
         for fun in self.command_functions:
             kwargs = match_grammars(command, fun.command_grammars)
             if kwargs is not False:
                 logging.info("Grammar Parsed:\n\tcommand: {0}\n\tmodule: {1}\n\targs: {2}".format(command, self.__module_name__ + " - " + fun.__name__, kwargs))
-                fun(self,user,**kwargs)
+                fun(self, user, **kwargs)
                 hit = True
         return hit
-    
-    def speak(self,user,message):
-        return self.bot.speak(user,message)
+
+    def speak(self, user, message):
+        return self.bot.speak(user, message)
