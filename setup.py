e184f88be03e437e09f61892cead165cc6552097
Craig Ferguson <craig.ferguson@babbaco.com>
2012-12-06 14:11:02 -0500

    small changes to setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Watson',
-    version='0.1dev',
-    packages=['watson',],
+    version='0.1.0',
+    packages=['watson','watson.modules','watson.tests'],
     license='LICENSE.txt',
     long_description=open('README.md').read(),
+    description="The Chatbot that won America's hearts in the 80's is back! And this time, he means business!",
     install_requires=[
         "Twisted >= 12.2.0",
         "pinder==1.0.1",
